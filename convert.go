d5bd63b92e51116a32f4042026f55cae334d7a5c
Thayne McCombs <astrothayne@gmail.com>
2019-07-22 00:17:29 -0600

    convert to hcl json

---
+++ convert.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hashicorp/hcl2/hcl"
+	"github.com/hashicorp/hcl2/hcl/hclsyntax"
+	"github.com/zclconf/go-cty/cty"
+	ctyconvert "github.com/zclconf/go-cty/cty/convert"
+	ctyjson "github.com/zclconf/go-cty/cty/json"
+	"strings"
+)
+
+type jsonObj map[string]interface{}
+
+type converter struct {
+	bytes []byte
+	diags hcl.Diagnostics
+}
+
+func (c *converter) rangeSource(r hcl.Range) string {
+	return string(c.bytes[r.Start.Byte:r.End.Byte])
+}
+
+func (c *converter) convertBody(body *hclsyntax.Body) (jsonObj, error) {
+	var err error
+	out := make(jsonObj)
+	for key, value := range body.Attributes {
+		out[key], err = c.convertExpression(value.Expr)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for _, block := range body.Blocks {
+		err = c.convertBlock(block, out)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return out, nil
+}
+
+func (c *converter) convertBlock(block *hclsyntax.Block, out jsonObj) error {
+	var key string = block.Type
+
+	value, err := c.convertBody(block.Body)
+	if err != nil {
+		return err
+	}
+
+	for _, label := range block.Labels {
+		if inner, exists := out[key]; exists {
+			var ok bool
+			out, ok = inner.(jsonObj)
+			if !ok {
+				// TODO: better diagnostics
+				return fmt.Errorf("Unable to conver Block to JSON: %v.%v", block.Type, strings.Join(block.Labels, "."))
+			}
+		} else {
+			obj := make(jsonObj)
+			out[key] = obj
+		}
+		key = label
+	}
+
+	if current, exists := out[key]; exists {
+		if list, ok := current.([]interface{}); ok {
+			out[key] = append(list, value)
+		} else {
+			out[key] = []interface{}{current, value}
+		}
+	} else {
+		out[key] = value
+	}
+
+	return nil
+}
+
+func (c *converter) convertExpression(expr hclsyntax.Expression) (interface{}, error) {
+	// assume it is hcl syntax (because, um, it is)
+	switch value := expr.(type) {
+	case *hclsyntax.LiteralValueExpr:
+		return ctyjson.SimpleJSONValue{Value: value.Val}, nil
+	case *hclsyntax.TemplateExpr:
+		return c.convertTemplate(value)
+	case *hclsyntax.TemplateWrapExpr:
+		return c.convertExpression(value.Wrapped)
+	case *hclsyntax.TupleConsExpr:
+		var list []interface{}
+		for _, ex := range value.Exprs {
+			elem, err := c.convertExpression(ex)
+			if err != nil {
+				return nil, err
+			}
+			list = append(list, elem)
+		}
+		return list, nil
+	case *hclsyntax.ObjectConsExpr:
+		m := make(jsonObj)
+		for _, item := range value.Items {
+			key, err := c.convertKey(item.KeyExpr)
+			if err != nil {
+				return nil, err
+			}
+			m[key], err = c.convertExpression(item.ValueExpr)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return m, nil
+	default:
+		return c.wrapExpr(expr), nil
+	}
+}
+
+func (c *converter) convertTemplate(t *hclsyntax.TemplateExpr) (string, error) {
+	if t.IsStringLiteral() {
+		// safe because the value is just the string
+		v, err := t.Value(nil)
+		if err != nil {
+			return "", err
+		}
+		return v.AsString(), nil
+	}
+	var builder strings.Builder
+	for _, part := range t.Parts {
+		s, err := c.convertStringPart(part)
+		if err != nil {
+			return "", err
+		}
+		builder.WriteString(s)
+	}
+	return builder.String(), nil
+}
+
+func (c *converter) convertStringPart(expr hclsyntax.Expression) (string, error) {
+	switch v := expr.(type) {
+	case *hclsyntax.LiteralValueExpr:
+		s, err := ctyconvert.Convert(v.Val, cty.String)
+		if err != nil {
+			return "", err
+		}
+		return s.AsString(), nil
+	case *hclsyntax.TemplateExpr:
+		return c.convertTemplate(v)
+	case *hclsyntax.TemplateWrapExpr:
+		return c.convertStringPart(v.Wrapped)
+	case *hclsyntax.ConditionalExpr:
+		return c.convertTemplateConditional(v)
+	case *hclsyntax.TemplateJoinExpr:
+		return c.convertTemplateFor(v.Tuple.(*hclsyntax.ForExpr))
+	default:
+		// treating as an embedded expression
+		fmt.Printf("Using default for %T\n", expr)
+		return c.wrapExpr(expr), nil
+	}
+}
+
+func (c *converter) convertKey(keyExpr hclsyntax.Expression) (string, error) {
+	// a key should never have dynamic input
+	if k, isKeyExpr := keyExpr.(*hclsyntax.ObjectConsKeyExpr); isKeyExpr {
+		keyExpr = k.Wrapped
+		if _, isTraversal := keyExpr.(*hclsyntax.ScopeTraversalExpr); isTraversal {
+			return c.rangeSource(keyExpr.Range()), nil
+		}
+	}
+	return c.convertStringPart(keyExpr)
+}
+
+func (c *converter) convertTemplateConditional(expr *hclsyntax.ConditionalExpr) (string, error) {
+	var builder strings.Builder
+	builder.WriteString("%{if ")
+	builder.WriteString(c.rangeSource(expr.Condition.Range()))
+	builder.WriteString("}")
+	trueResult, err := c.convertStringPart(expr.TrueResult)
+	if err != nil {
+		return "", nil
+	}
+	builder.WriteString(trueResult)
+	falseResult, err := c.convertStringPart(expr.FalseResult)
+	if len(falseResult) > 0 {
+		builder.WriteString("%{else}")
+		builder.WriteString(falseResult)
+	}
+	builder.WriteString("%{endif}")
+
+	return builder.String(), nil
+}
+
+func (c *converter) convertTemplateFor(expr *hclsyntax.ForExpr) (string, error) {
+	var builder strings.Builder
+	builder.WriteString("%{for ")
+	if len(expr.KeyVar) > 0 {
+		builder.WriteString(expr.KeyVar)
+		builder.WriteString(", ")
+	}
+	builder.WriteString(expr.ValVar)
+	builder.WriteString(" in ")
+	builder.WriteString(c.rangeSource(expr.CollExpr.Range()))
+	builder.WriteString("}")
+	templ, err := c.convertStringPart(expr.ValExpr)
+	if err != nil {
+		return "", err
+	}
+	builder.WriteString(templ)
+	builder.WriteString("%{endfor}")
+
+	return builder.String(), nil
+}
+
+func (c *converter) wrapExpr(expr hclsyntax.Expression) string {
+	return "${" + c.rangeSource(expr.Range()) + "}"
+}
--- main.go
+++ main.go
@@ -2,33 +2,73 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"github.com/hashicorp/hcl2/hcl"
+	"github.com/hashicorp/hcl2/hcl/hclsyntax"
 	"github.com/hashicorp/hcl2/hclpack"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
+var usePackFormat bool
+
+func init() {
+	flag.BoolVar(&usePackFormat, "pack", false, "If set, use the pack json format, instead of valid hcl json")
+}
+
 func main() {
 	logger := log.New(os.Stderr, "", 0)
-	f := os.Args[1]
-	bytes, err := ioutil.ReadFile(f)
-	if err != nil {
-		logger.Fatalf("Uh oh: %s\n", err)
+	flag.Parse()
+	var err error
+
+	var filename = flag.Arg(0)
+	if filename == "" {
+		logger.Fatalf("Usage: must supply a file to convert")
 	}
 
-	body, diags := hclpack.PackNativeFile(bytes, f, hcl.Pos{Line: 1, Column: 1})
-
-	if diags.HasErrors() {
-		logger.Fatalf("Failed to parse: %s", diags.Error())
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		logger.Fatalf("Failed to read file: %s\n", err)
 	}
 
-	jb, err := json.MarshalIndent(body, "", "    ")
+	var content interface{}
+	if usePackFormat {
+		content, err = getPackJSON(bytes, filename)
+	} else {
+		content, err = getHclJSON(bytes, filename)
+	}
+
 	if err != nil {
-		logger.Fatalf("Failed to marshal: %v", err)
+		logger.Fatalf("Failed to convert file: %v", err)
+	}
+
+	jb, err := json.MarshalIndent(content, "", "    ")
+
+	if err != nil {
+		logger.Fatalf("Failed to generate JSON: %v", err)
 	}
 
 	os.Stdout.Write(jb)
 
 	// ioutil.WriteFile(f+".json", jb, 0666)
+}
+
+func getPackJSON(bytes []byte, filename string) (interface{}, error) {
+	f, diags := hclpack.PackNativeFile(bytes, filename, hcl.Pos{Line: 1, Column: 1})
+	if diags.HasErrors() {
+		return nil, diags
+	}
+	return f, nil
+}
+
+func getHclJSON(bytes []byte, filename string) (interface{}, error) {
+	file, diags := hclsyntax.ParseConfig(bytes, filename, hcl.Pos{Line: 1, Column: 1})
+	if diags.HasErrors() {
+		return nil, diags
+	}
+	body := file.Body.(*hclsyntax.Body)
+
+	c := converter{bytes: bytes}
+	return c.convertBody(body)
 }
