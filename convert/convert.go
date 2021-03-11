2a0fd3b90b5899f4502324253d5101282a572861
Taras Brodovych <skydion@ukr.net>
2021-03-11 09:08:50 +0100

    Don't wrap internal variable types into '${...}' string

--- convert/convert.go
+++ convert/convert.go
@@ -195,6 +195,13 @@ func (c *converter) convertExpression(expr hclsyntax.Expression) (interface{}, e
 		}
 		return m, nil
 	default:
+		val := c.rangeSource(expr.Range())
+		types := []string{"string", "number", "bool", "list", "set", "map", "object", "tuple"}
+		for _, t := range types {
+			if res := strings.HasPrefix(val, t); res {
+				return val, nil
+			}
+		}
 		return c.wrapExpr(expr), nil
 	}
 }
